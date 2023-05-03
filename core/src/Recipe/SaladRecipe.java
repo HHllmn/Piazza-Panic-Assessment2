a0970e6d31196baa121a5392258485dc20ade91e
MedicCrunch <jonathon.snelgrove@gmail.com>
2023-05-03 02:48:19 +0100

    Code Cleaning for: Bin SaladRecipe

--- core/src/Recipe/SaladRecipe.java
+++ core/src/Recipe/SaladRecipe.java
@@ -1,22 +1,33 @@
+/**
+ *  SaladRecipe is a recipe in the game.
+ */
 package Recipe;
 
-import Ingredients.*;
+import Ingredients.Lettuce;
+import Ingredients.Tomato;
+import Ingredients.Onion;
 import com.badlogic.gdx.graphics.Texture;
 import java.util.ArrayList;
 
 /**
  * The SaladRecipe class is a subclass of the Recipe class.
- * It holds an ArrayList of ingredients that makes up a salad dish, and the texture of the completed salad dish.
- * The salad dish consists of {@link Ingredients.Lettuce}, {@link Ingredients.Tomato} and {@link Ingredients.Onion} ingredients.
+ * It holds an ArrayList of ingredients that makes up a salad
+ * dish, and the texture of the completed salad dish.
+ * <p>
+ * The salad dish consists of {@link Ingredients.Lettuce},
+ * {@link Ingredients.Tomato} and {@link Ingredients.Onion} ingredients.
  */
 
+/**
+ * Constructor for the SaladRecipe class.
+ */
 public class SaladRecipe extends Recipe {
-    public SaladRecipe(){
+    public SaladRecipe() {
         super.ingredients = new ArrayList<>();
         super.amIReady = true;
-        ingredients.add(new Lettuce(0,0));
-        ingredients.add(new Tomato(0,0));
-        ingredients.add(new Onion(0,0));
+        ingredients.add(new Lettuce(0, 0));
+        ingredients.add(new Tomato(0, 0));
+        ingredients.add(new Onion(0, 0));
         completedImg = new Texture("Food/Salad.png");
     }
 }
--- core/src/Sprites/Bin.java
+++ core/src/Sprites/Bin.java
@@ -1,3 +1,6 @@
+/**
+ *  Bin is a workstation in the game.
+ */
 package Sprites;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -6,14 +9,23 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 /**
- * Bin is a concrete class that extends the {@link InteractiveTileObject} class.
- * It creates a bin object that interacts with other objects in the game world.
+ * Bin is a concrete class that extends the {@link InteractiveTileObject}
+ * class. It creates a bin object that interacts with other objects in
+ * the game world.
  */
-
 public class Bin extends InteractiveTileObject {
+
+    /**
+     * Constructor for Bin.
+     * Creates a new Bin with a Box2D body and fixture.
+     *
+     * @param world The playable world.
+     * @param map The tiled map.
+     * @param bdef The body definition of a tile.
+     * @param rectangle Rectangle shape.
+     */
     public Bin(World world, TiledMap map, BodyDef bdef, Rectangle rectangle) {
         super(world, map, bdef, rectangle);
         fixture.setUserData(this);
-
     }
 }
